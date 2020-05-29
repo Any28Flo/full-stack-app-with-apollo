e26024b286cd6266dea9d3bd375f3c747852fa3e
Daedra <foglzerika@gmail.com>
2020-05-29 05:36:24 -0500

    feat | Adding query, mutation types to our schema and configure our server

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,38 +1,46 @@
 const { gql } = require('apollo-server');
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
 
 const typeDefs = gql `
+    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
     #Your schemas goes here
     type Launch {
         id :ID!
         site : String
-        mission : Mission
-        rocket : Rocket
         isBooked  : Boolean!
     }
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
     type User {
         id: ID!
-        email : String
-        trips : [Launch]!
+        email: String!
+        trips: [Launch]!
     }
-    }
-    type Mission {
-        name : String
-        missionPatch( size: PatchSize) : String
-    }
-    type Rocket{
-        id : ID!
-        name : String
-        type : String
 
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
     }
-    enum PatchSize{
+
+    enum PatchSize {
         SMALL
         LARGE
     }
+    # The "Query" type is special: it lists all of the available queries that
+    # clients can execute, along with the return type for each. In this
+    # case, the "books" query returns an array of zero or more Books (defined above).
+
     type Query {
-        launches : [Launch]!
-        launch(id : ID!) : Launch
-        me : User
+        launches: [Launch]!
+        launch(id: ID!): Launch
+        me: User
     }
     type Mutation {
         bookTrips(launchIds: [ID]!): TripUpdateResponse!
