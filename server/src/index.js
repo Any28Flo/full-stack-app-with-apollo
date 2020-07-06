28d8cc9022ded7d5e9da1b074cf9e1ffd4328c5b
Daedra <foglzerika@gmail.com>
2020-07-06 08:17:19 -0500

    feat | Create resolvers and schemas

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,35 @@
-const { ApolloServer, gql } = require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require('apollo-server');
+const {typeDefs, resolvers} = require('./schema');
+/*
+const { createStore } = require('./utilis.js');
+
+
+const UserAPI = require('./datasources/user');
+*/
+
+/* Import and cal  createStore function to set up our SQLite database*/
+//const store = createStore();
+
+
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs});
+/* Adding dataSources function to connect instances of LaunchAPI and UserAPi */
+/*
+
+const server = new ApolloServer({
+    typeDefs,
+    dataSources : () =>({
+        launchAPI : new LaunchAPI(),
+        userAPI : new UserAPI({store})
+    })
+});
+*/
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+server.listen(4000)
+    .then( () =>   console.log(`🚀 Server ready at 4000`))
--- server/src/schema.js
+++ server/src/schema.js
@@ -10,6 +10,8 @@ const typeDefs = gql `
     type Launch {
         id :ID!
         site : String
+        mission: Mission
+        rocket: Rocket
         isBooked  : Boolean!
     }
     type Rocket {
@@ -21,14 +23,16 @@ const typeDefs = gql `
     type User {
         id: ID!
         email: String!
+        avatar : String
+        friends :[User]
         trips: [Launch]!
     }
-
+#
     type Mission {
         name: String
         missionPatch(size: PatchSize): String
     }
-
+#
     enum PatchSize {
         SMALL
         LARGE
@@ -36,21 +40,70 @@ const typeDefs = gql `
     # The "Query" type is special: it lists all of the available queries that
     # clients can execute, along with the return type for each. In this
     # case, the "books" query returns an array of zero or more Books (defined above).
-
-    type Query {
-        launches: [Launch]!
-        launch(id: ID!): Launch
-        me: User
+    #Define operations clients can perform to access data that resembles 
+    #the shape of the other Types in the Schema
+    #*****CREATING QUERIES****
+    # 1. Create Query Type in the Chema using SDL
+    #2. Add fields to the Query Type
+    #3. Create Resolvers  that for the  fields
+    
+    
+    type Query{
+        me: User!,
+        launches: [Launch]!,
+        launch(id: ID!): Launch,
+        rockets : Rocket!
     }
-    type Mutation {
-        bookTrips(launchIds: [ID]!): TripUpdateResponse!
-        cancelTrip(launchId: ID!): TripUpdateResponse!
-        login(email: String): String # login token
-    }
-    type TripUpdateResponse {
-        success: Boolean!
-        message: String
-        launches: [Launch]
-    }
+    
+#    type Query {
+#        
+#        me: User
+#    }
+#    type Mutation {
+#        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+#        cancelTrip(launchId: ID!): TripUpdateResponse!
+#        login(email: String): String # login token
+#    }
+#    type TripUpdateResponse {
+#        success: Boolean!
+#        message: String
+#        launches: [Launch]
+#    }
 `;
-module.exports = typeDefs;
+//
+// Functions that are responsible for returning values for
+// fields that exist on Tyoes in a Schema. Resolvers execution is
+// dependent on th incomint clien Queyr
+// ***CREATING RESOLVERS
+// 1.Resolver names must match the exact field name on your Schemas types
+// 2.Resolvers must return the value type declared for the marhing field
+// 3.Resolvers can be async
+// 4.Can retrieve data from any source
+/***** ARGUMENTS */
+//Allow clients to pass data variables with Queries that can
+// be used in your Resolvers to get data
+/* 1. Must be defined in our Schema
+2. Can be added to any field
+3. Either have to be Scalars or Input types*/
+/* *** Input Types  */
+/*Just like Types, but used for Arguments
+All field value types must be other Input Types or Scalars*/
+const resolvers ={
+    Query :{
+        me(){
+            return {
+                email : 'yoda@masters.com',
+                avatar : 'http://yoda.png',
+                friends : []
+            }
+        },
+        rockets(){
+          return{
+              name: "FALCON 9",
+              type : "Capsule"
+
+          }
+        }
+    }
+}
+module.exports = {typeDefs , resolvers};
